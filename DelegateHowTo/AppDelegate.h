c47ab64d66441c1ac6730614e658d549c03b23d1
KPicart <kpicart@stepwisedesigns.com>
2015-10-07 23:46:50 -0400

    delegate practice-YouTubeVid

---
+++ DelegateHowTo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DelegateHowTo
+//
+//  Created by Krishna Picart on 10/7/15.
+//  Copyright (c) 2015 stepwisedesigns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DelegateHowTo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DelegateHowTo
+//
+//  Created by Krishna Picart on 10/7/15.
+//  Copyright (c) 2015 stepwisedesigns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
