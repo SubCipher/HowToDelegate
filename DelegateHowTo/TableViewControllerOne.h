f6f8b062ab1a8a964ebdb39a422ae6c28c69d267
KPicart <kpicart@stepwisedesigns.com>
2015-10-09 23:35:34 -0400

    delegation

---
+++ DelegateHowTo/TableViewControllerOne.h
@@ -0,0 +1,13 @@
+//
+//  TableViewControllerOne.h
+//  DelegateHowTo
+//
+//  Created by MacMan on 10/9/15.
+//  Copyright © 2015 stepwisedesigns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewControllerOne : UITableViewController
+
+@end
---
+++ DelegateHowTo/ViewControllerOne.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerOne.h
+//  DelegateHowTo
+//
+//  Created by MacMan on 10/9/15.
+//  Copyright © 2015 stepwisedesigns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerOne : UIViewController
+
+@end
