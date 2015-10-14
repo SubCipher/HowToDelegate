fd2da45c5835e3dd750df3ecd9038e5b1243cc38
KPicart <kpicart@stepwisedesigns.com>
2015-10-13 23:52:24 -0400

    alwaysBeCoding

---
+++ DelegateHowTo/DepartmentsTableVC.h
@@ -0,0 +1,13 @@
+//
+//  DepartmentsTableVC.h
+//  DelegateHowTo
+//
+//  Created by MacMan on 10/13/15.
+//  Copyright © 2015 stepwisedesigns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DepartmentsTableVC : UITableViewController
+
+@end
